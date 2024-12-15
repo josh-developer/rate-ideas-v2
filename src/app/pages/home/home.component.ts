e0f20124495128e3f1b318b33adb728a9967e947
joshdeveloper <joshdeveloper22@gmail.com>
2024-12-16 00:17:21 +0500

    Home page #1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { TabComponent } from '../../shared/components/tab/tab.component';
+import { ITabData } from '../../shared/models/frontend/tab';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, TabComponent, FormsModule],
   templateUrl: './home.component.html',
 })
 export default class HomeComponent {
+  tabs = signal([ { label: 'Tab 1', id: 1 }, { label: 'Tab 2', id: 2 } ]);
+  activeTab = signal<string>('');
+  inputText = 'TEST';
 
+  onTabsChange(tab: ITabData): void {
+    console.log("Ilvoldim, bratim: ", tab);   
+    
+    this.activeTab.set(tab.label);
+  }
 }
---
+++ src/app/shared/components/tab/tab.component.ts
@@ -0,0 +1,19 @@
+import { Component, input, output } from '@angular/core';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { ITabData } from '../../models/frontend/tab';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: 'tab.component.html',
+  styleUrl: 'tab.component.scss',
+  imports: [MatTabsModule],
+})
+export class TabComponent {
+  tabs = input<ITabData[]>([]);
+  tabChange = output<ITabData>();
+
+  onTabChange(tab: MatTabChangeEvent): void {
+    const selectedTab = this.tabs()[tab.index];
+    this.tabChange.emit(selectedTab);
+  }
+}
